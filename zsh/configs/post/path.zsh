f7ca30bbab75c65a54b28e0d15579d4c0a7d4197
Valentino Stoll <vstoll@doximity.com>
2019-02-12 14:52:27 -0500

    Moves all PATH setting to proper zsh/configs/post/path.zsh

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,5 +1,30 @@
 # ensure dotfiles bin directory is loaded first
-PATH="$HOME/.bin:/usr/local/sbin:$PATH"
+PATH="$HOME/.bin:/usr/local/bin:/usr/local/sbin:$PATH"
+
+# load homebrew if available
+if type /usr/local/bin/brew &>/dev/null ; then
+  # Add homebrew completions
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+fi
+
+# load rbenv if available
+if type rbenv &>/dev/null ; then
+  eval "$(rbenv init - --no-rehash)"
+elif type $HOME/.rvm/scripts/rvm &>/dev/null ; then
+  export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+fi
+
+# add yarn if available
+if type yarn &>/dev/null ; then
+  export PATH="$PATH:`yarn global bin`"
+fi
+
+# add go if available
+if type go &>/dev/null ; then
+  export PATH="$PATH:$HOME/go/bin"
+  export GOPATH=$(go env GOPATH)
+fi
 
 # load ASDF, falling back to rbenv if not available
 if [ -d "$HOME/.asdf" ]; then
